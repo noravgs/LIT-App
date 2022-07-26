bee8463d5ddb8a2417d8dae8729444447083cd8c
Nora Vargas <noravgs92@gmail.com>
2022-07-26 15:01:19 -0500

    procfile changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const cors = require('cors')
-const PORT = 2121
+const PORT = process.env.PORT || 2121
 
 app.use(cors())
 
